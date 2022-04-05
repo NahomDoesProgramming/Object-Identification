e94ea8d2cf0ea797c1a0b1dbaf85fe40a2904712
NahomDoesProgramming <84427393+NahomDoesProgramming@users.noreply.github.com>
2022-04-04 18:37:14 -0700

    Add files via upload
    v

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
+img = "";
+status = "";
 function setup()
 {
     canvas = createCanvas(640, 420);
     canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detecting Objects";
 }
 function preload()
 {
@@ -15,4 +19,24 @@ function draw()
     noFill();
     stroke("#2BE");
     rect(30, 60, 450, 350);
+
+    fill("#FF0000");
+    text("Cat", 320, 120);
+    noFill();
+    stroke("#FF0000");
+    rect(300, 90, 270, 320);
+}
+function modelLoaded()
+{
+    console.log("The model successfully loaded!");
+    status = true;
+    objectDetector.detect(img, gotResult);
+}
+function gotResult(error, results)
+{
+    if(error)
+    {
+        console.log(error);
+    }
+    console.log(results);
 }
